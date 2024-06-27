c55bd1119c2daebebacadd38766a5b3f264f7779
PrincewillDev <princewillfidelis1@gmail.com>
2024-06-28 00:35:28 +0100

    Updated do_create function to support object creation with parameters

--- console.py
+++ console.py
@@ -11,6 +11,19 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
+def processedValue(value):
+    # Process float values
+    if '.' in value and not value.startswith('"'):
+        return float(value)
+
+    elif value.isdigit() or (value[0] == '-' and value[1:].isdigit()):
+        return int(value)
+    elif value.startswith('"'):
+        formatted_str = value.strip('"').replace('"', '\"').replace('_', ' ')
+        return formatted_str
+    else:
+        return None
+
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
@@ -73,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +128,33 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        classname, *params = args.split()
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif classname not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        
+        new_instance = HBNBCommand.classes[classname]()
+        if params:
+            for param in params:
+                    try:
+                        # split the parameters to key=value
+                        key, value = param.split('=', 1)
+                        # process the values
+                        val = processedValue(value)
+                        # set the params as attribut to the new instance
+                        setattr(new_instance, key, val)        
+                    # use except to track valuerror
+                    except ValueError:
+                        continue
+        
+        # call storage object and add the new_instance to storage engine
+        storage.new(new_instance)
+        # Save storage
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +302,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +310,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import os
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """This class manages storage of hbnb models in JSON format"""    
+    __file_path = os.path.join("./models/engine","file.json")
     __objects = {}
 
     def all(self):
